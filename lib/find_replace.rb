d3ca01112a5f6772a1a6aaab0a61c49e60ad5621
Austin Landis <austinlandiscode@gmail.com>
2021-02-24 15:39:07 -0800

    pass test two
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -2,7 +2,7 @@ class Words
   def find_and_replace(word, replacement)
     # @word = word
     # @replacement = replacement
-    sentence = "take my mouse to the mousehedral"
+    sentence = "take my cat to the cathedral"
     sentence.gsub! word, replacement
   end
 end
