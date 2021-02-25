a6da6bde1773b97d7a82a2c53d8ce0142e48da88
Austin Landis <austinlandiscode@gmail.com>
2021-02-24 16:21:52 -0800

    script file
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,8 +1,8 @@
 class Words
+  word = ""
+  replacement = ""
   def find_and_replace(word, replacement)
     sentence = gets.chomp
-    word = gets.chomp
-    replacement = gets.chomp
     sentence.gsub! word, replacement
   end
 end
